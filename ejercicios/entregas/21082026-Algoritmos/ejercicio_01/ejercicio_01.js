d969028cdbbcc59af70104c076da801d3719fd4a
Fer_IMC <l.martinez@paimc.mx>
2026-08-21 15:26:09 -0600

    creación de los 3 algoritmos con archivos .txt y código en js

---
+++ ejercicios/entregas/21082026-Algoritmos/ejercicio_01/ejercicio_01.js
@@ -0,0 +1,8 @@
+// Calcular el perímetro de un círculo (Math.PI)
+function calcularPerimetroCirculo(radio) {
+    let perimetro = 2 * Math.PI * radio;
+    return perimetro;
+}
+
+console.log("Perímetro de círculo con radio 5:", calcularPerimetroCirculo(5));
+
---
+++ ejercicios/entregas/21082026-Algoritmos/ejercicio_03/ejercicio_03.js
@@ -0,0 +1,7 @@
+// Calcular la raíz cuadrada de un número (Math.sqrt)
+function calcularRaizCuadrada(numero) {
+    let resultado = Math.sqrt(numero);
+    return resultado;
+}
+
+console.log("Raíz cuadrada de 25:", calcularRaizCuadrada(25));
