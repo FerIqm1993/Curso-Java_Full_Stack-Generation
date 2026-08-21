8c13b4f5a65482c31070a93764ff23c3aa511ccc
Fer_IMC <l.martinez@paimc.mx>
2026-08-21 13:38:53 -0600

    ajuste en organización de carpeta

--- clases/01_funciones.js
+++ clases/01_funciones.js
@@ -13,3 +13,13 @@ let ladoB = 10;
 // Mostrar mensaje y función en consola
 console.log(`El área de un rectángulo de base ${ladoA} unidades y altura ${ladoB} unidades es de ${calcularAreaRectangulo(ladoA, ladoB)} unidades cuadradas.`);
 
+// Función "arrow"
+const calcularAreaRectangulo = (base, altura) => base * altura;
+
+// Definir variables de lados a usar
+let ladoA = 5;
+let ladoB = 10;
+
+// Mostrar mensaje y función en consola
+console.log(`El área de un rectángulo de base ${ladoA} unidades y altura ${ladoB} unidades es de ${calcularAreaRectangulo(ladoA, ladoB)} unidades cuadradas.`);
+
